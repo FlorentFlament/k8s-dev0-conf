f53e6c75e83b947411c521a2fd506d2f782d5143
Florent Flament <contact@florentflament.com>
2024-10-30 21:15:54 +0100

    Update bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,17 +1,13 @@
-# Create a kind cluster (or use a K8s cluster anywhere)
-kind create cluster
+#!/bin/sh
+set -eux
 
-# Install basic argocd - no custom variables
+kind create cluster --config kind.yaml
+
 helm install argocd bitnami/argo-cd -n argocd --create-namespace
 
-# Expose argocd via port forwarding (Or do that with k9s)
-kubectl -n argocd port-forward svc/argocd-argo-cd-server 8443:443 &
+kubectl wait --namespace argocd \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=server \
+  --timeout=300s
 
-# Allow argocd to connect to private git repo
-argocd login localhost:8443 \
-       --username admin \
-       --password $(kubectl -n argocd get secret argocd-secret -o json | jq -r .data.clearPassword | base64 -d) \
-       --insecure
-
-# Plug argoCD to our configuration repo
 kubectl apply -f apps.yaml
